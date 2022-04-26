3a17a5db23a3a3e769fc1f6de211ba77563990e4
An An <2236368544@qq.com>
2022-04-26 20:56:36 +0800

    Create AATreemap.py

---
+++ aacharts/aaoptionsmodelpro/AATreemap.py
@@ -0,0 +1,21 @@
+# //
+# //  AATreemap.swift
+# //  AAInfographics-ProDemo
+# //
+# //  Created by AnAn on 2020/11/10.
+# //
+#
+# import UIKit
+
+class AATreemap:
+    layoutAlgorithm: str
+    allowTraversingTree: bool
+    
+    def layoutAlgorithm(self, prop: str):
+        layoutAlgorithm = prop
+        return self
+    
+    def allowTraversingTree(self, prop: bool):
+        allowTraversingTree = prop
+        return self
+
