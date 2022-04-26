003c36f865e216c9a7f8c6bcdf1eb8bf999c9ca9
An An <2236368544@qq.com>
2022-04-26 20:29:27 +0800

    Create AAOptionsData.py

---
+++ demo/AAOptionsData.py
@@ -0,0 +1,126 @@
+class AAOptionsData:
+    @staticmethod
+    def variablepieData():
+        return AAOptionsData.getJsonDataWithJsonFileName("variablepieData")
+
+    @staticmethod
+    def variwideData():
+        return AAOptionsData.getJsonDataWithJsonFileName("variwideData")
+
+    @staticmethod
+    def heatmapData():
+        return AAOptionsData.getJsonDataWithJsonFileName("heatmapData")
+
+    @staticmethod
+    def columnpyramidData():
+        return AAOptionsData.getJsonDataWithJsonFileName("columnpyramidData")
+
+    @staticmethod
+    def treemapWithColorAxisData():
+        return AAOptionsData.getJsonDataWithJsonFileName("treemapWithColorAxisData")
+
+    @staticmethod
+    def drilldownTreemapData():
+        return AAOptionsData.getJsonDataWithJsonFileName("drilldownTreemapData")
+
+    @staticmethod
+    def sankeyData():
+        return AAOptionsData.getJsonDataWithJsonFileName("sankeyData")
+
+    @staticmethod
+    def dependencywheelData():
+        return AAOptionsData.getJsonDataWithJsonFileName("dependencywheelData")
+
+    @staticmethod
+    def sunburstData():
+        return AAOptionsData.getJsonDataWithJsonFileName("sunburstData")
+
+    @staticmethod
+    def dumbbellData():
+        return AAOptionsData.getJsonDataWithJsonFileName("dumbbellData")
+
+    @staticmethod
+    def vennData():
+        return AAOptionsData.getJsonDataWithJsonFileName("vennData")
+
+    @staticmethod
+    def lollipopData():
+        return AAOptionsData.getJsonDataWithJsonFileName("lollipopData")
+
+    @staticmethod
+    def tilemapData():
+        return AAOptionsData.getJsonDataWithJsonFileName("tilemapData")
+
+    @staticmethod
+    def treemapWithLevelsData():
+        return AAOptionsData.getJsonDataWithJsonFileName("treemapWithLevelsData")
+
+    @staticmethod
+    def bellcurveData():
+        return AAOptionsData.getJsonDataWithJsonFileName("bellcurveData")
+
+    @staticmethod
+    def timelineData():
+        return AAOptionsData.getJsonDataWithJsonFileName("timelineData")
+
+    @staticmethod
+    def itemData():
+        return AAOptionsData.getJsonDataWithJsonFileName("itemData")
+
+    @staticmethod
+    def windbarbData():
+        return AAOptionsData.getJsonDataWithJsonFileName("windbarbData")
+
+    @staticmethod
+    def networkgraphData():
+        return AAOptionsData.getJsonDataWithJsonFileName("networkgraphData")
+
+    @staticmethod
+    def wordcloudData():
+        return AAOptionsData.getJsonDataWithJsonFileName("wordcloudData")
+
+    @staticmethod
+    def eulerData():
+        return AAOptionsData.getJsonDataWithJsonFileName("eulerData")
+
+    @staticmethod
+    def organizationData():
+        return AAOptionsData.getJsonDataWithJsonFileName("organizationData")
+
+    @staticmethod
+    def organizationNodesData():
+        return AAOptionsData.getJsonDataWithJsonFileName("organizationNodesData")
+
+    @staticmethod
+    def arcdiagram1Data():
+        return AAOptionsData.getJsonDataWithJsonFileName("arcdiagram1Data")
+
+    @staticmethod
+    def arcdiagram2Data():
+        return AAOptionsData.getJsonDataWithJsonFileName("arcdiagram2Data")
+
+    @staticmethod
+    def arcdiagram3Data():
+        return AAOptionsData.getJsonDataWithJsonFileName("arcdiagram3Data")
+
+    @staticmethod
+    def flameData():
+        return AAOptionsData.getJsonDataWithJsonFileName("flameData")
+
+    @staticmethod
+    def sunburst2Data():
+        return AAOptionsData.getJsonDataWithJsonFileName("sunburst2Data")
+
+    @staticmethod
+    def xrangeData():
+        return AAOptionsData.getJsonDataWithJsonFileName("sunburst2Data")
+
+
+    @staticmethod
+    def vectorData():
+        return AAOptionsData.getJsonDataWithJsonFileName("vectorData")
+
+
+    @staticmethod
+    def getJsonDataWithJsonFileName(jsonFileName: str):
+        return ""
