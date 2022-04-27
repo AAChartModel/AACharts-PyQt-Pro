2a4514a5ec503344eb17f29de5528ae3394f6dff
An An <2236368544@qq.com>
2022-04-27 21:31:11 +0800

    Update AAOptionsSeries.py

--- demo/AAOptionsSeries.py
+++ demo/AAOptionsSeries.py
@@ -1,12 +1,22 @@
+import json
+
+
 class AAOptionsSeries:
     @staticmethod
     def packedbubbleSeries():
-        return AAOptionsSeries.getJsonDataWithJsonFileName("variablepieData")
+        return AAOptionsSeries.getJsonDataWithJsonFileName("packedbubbleSeries")
 
     @staticmethod
     def streamgraphSeries():
-        return AAOptionsSeries.getJsonDataWithJsonFileName("variwideData")
+        return AAOptionsSeries.getJsonDataWithJsonFileName("streamgraphSeries")
 
     @staticmethod
     def getJsonDataWithJsonFileName(jsonFileName: str):
-        return ""
+        # /Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/arcdiagram1Data.json
+        print("正在读取文件" + jsonFileName + "😊")
+        filePath = "/Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/seriesjson/" + jsonFileName + ".json"
+        # 打开一个json文件
+        data = open(filePath, encoding='utf-8')
+        # 转换为python对象
+        strJson = json.load(data)
+        return strJson
