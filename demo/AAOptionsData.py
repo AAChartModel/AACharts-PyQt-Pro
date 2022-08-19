be505bb26fb8346bb1e53b5b9f5cebad5a8fceb9
An An <2236368544@qq.com>
2022-08-19 16:57:23 +0800

    Change absolute file path to be relative file path

--- demo/AAOptionsData.py
+++ demo/AAOptionsData.py
@@ -1,4 +1,5 @@
 import json
+import os
 import random
 
 
@@ -158,9 +159,12 @@ class AAOptionsData:
     def getJsonDataWithJsonFileName(jsonFileName: str):
         # /Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/arcdiagram1Data.json
         print("正在读取文件" + jsonFileName + "😊")
-        filePath = "/Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/" + jsonFileName + ".json"
+        # filePath = "/Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/" + jsonFileName + ".json"
+        cwd = os.getcwd()
+        localJsonPath = cwd + "/demo/datajson/" + jsonFileName + ".json"
+        print("本地文件路径" + localJsonPath)
         # 打开一个json文件
-        data = open(filePath, encoding='utf-8')
+        data = open(localJsonPath, encoding='utf-8')
         # 转换为python对象
         strJson = json.load(data)
         return strJson
