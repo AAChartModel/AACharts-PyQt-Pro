8da267aa096507318a52f2d3d67b06d22919a2bc
An An <2236368544@qq.com>
2022-08-19 16:54:43 +0800

    Change CSV data file path to relative path

--- demo/AAOptionsCSV.py
+++ demo/AAOptionsCSV.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 
 class AAOptionsCSV:
@@ -11,9 +12,14 @@ class AAOptionsCSV:
     def getJsonDataWithJsonFileName(jsonFileName: map):
         # /Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/arcdiagram1Data.json
         print("正在读取文件" + jsonFileName + "😊")
-        filePath = "/Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/" + jsonFileName + ".json"
+        # filePath = "/Users/admin/Documents/GitHub/AACharts-PyQt-Pro/demo/datajson/" + jsonFileName + ".json"
+
+        cwd = os.getcwd()
+        localJsonPath = cwd + "/demo/datajson/" + jsonFileName + ".json"
+        print("本地文件路径" + localJsonPath)
+
         # 打开一个json文件
-        data = open(filePath, encoding='utf-8')
+        data = open(localJsonPath, encoding='utf-8')
         # 转换为python对象
         strJson = json.load(data)
         return strJson
